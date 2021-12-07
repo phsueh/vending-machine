1b6382f78c1408ffd66e2ee6002ddf5fd5898536
Paul Hsueh <hsueh.paul@gmail.com>
2021-12-07 13:08:11 -0500

    added coins test

--- spec/coin.spec.rb
+++ spec/coin.spec.rb
@@ -5,9 +5,8 @@ describe Coin do
     let(:coin) { described_class.new(name: "Two Dollars", value: 2, quantity: 5) }
   
     describe '#initialize' do
-      it 'should initialize with name, value, quantity' do
+      it 'initialize with name, value, quantity' do
         expect(coin).to have_attributes(:value => 2, :name => "Two Dollars", :quantity 5)
       end
     end
   end
-
